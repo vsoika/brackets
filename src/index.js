5c023eaf9e21a03d198c2646eea89e108be260ec
Volha Soika <volhasoika@gmail.com>
2019-09-29 19:01:16 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let symbols = str.split('');
+    let result = [];
+
+    for (let i = 0; i < symbols.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            const lastSymbol = result[result.length-1];
+            const isConfigSymbolsEqual = bracketsConfig[j][0] === bracketsConfig[j][1];
+            const isConfigSymbolEqualToLastSymbol = bracketsConfig[j][0] === lastSymbol;
+
+            if (symbols[i] === bracketsConfig[j][0]) {
+                if (isConfigSymbolsEqual && isConfigSymbolEqualToLastSymbol) {
+                   result.pop();
+                } else {
+                    result.push(symbols[i]);
+                }
+                break;
+            } else if (symbols[i] === bracketsConfig[j][1]) {
+                const pair = bracketsConfig[j][0];
+
+                if (lastSymbol === pair) {
+                    result.pop();
+                }
+            }
+        }
+    } 
+    
+    if (result.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
